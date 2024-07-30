fe1e535a4bdd0f847e9f0ea27cb09c107a59e703
MdTanvirHossainTusher <md.tanvir.hossain.tusher@gmail.com>
2024-07-30 21:43:57 +0600

    dependency removed

--- src/main/java/com/task/user_management/controller/HelloWorldController.java
+++ src/main/java/com/task/user_management/controller/HelloWorldController.java
@@ -2,8 +2,9 @@ package com.task.user_management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorldController {
     @GetMapping("/hello")
     public String helloWorld() {
