76563caa0f23c8fa8ece994f36a09158ddf00a69
MdTanvirHossainTusher <md.tanvir.hossain.tusher@gmail.com>
2024-07-30 19:08:28 +0600

    hello world added!

---
+++ src/main/java/com/task/user_management/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.task.user_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
