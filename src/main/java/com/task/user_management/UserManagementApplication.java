51aa9e0062edcff562f8ddf47b3db73e7173665c
Md Tanvir Hossain <md.tanvir.hossai.tusher>
2024-07-30 17:26:54 +0600

    properties added

---
+++ src/main/java/com/task/user_management/UserManagementApplication.java
@@ -0,0 +1,14 @@
+package com.task.user_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+}
